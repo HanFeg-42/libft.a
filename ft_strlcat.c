a7419c7dc004b44b8e8e93de496f3f8b902d9d82
HanFeg-42 <hananefegrachi2002@gmail.com>
2024-11-01 11:27:09 +0100

    Rename bonus part

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: hfegrach <hfegrach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 17:11:42 by marvin            #+#    #+#             */
-/*   Updated: 2024/10/31 22:41:36 by hfegrach         ###   ########.fr       */
+/*   Updated: 2024/11/01 10:15:24 by hfegrach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,30 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 	size_t	dstsize;
+	size_t	srcsize;
 
 	dstsize = ft_strlen(dst);
+	srcsize = ft_strlen(src);	
 	if (size <= dstsize)
-		return (size + ft_strlen(src));
+		return (size + srcsize);
 	i = 0;
-	while (i < size - 1 && src[i])
+	while (i < size -dstsize - 1 && src[i])
 	{
 		dst[dstsize  + i] = src[i];
 		i++;
 	}
 	dst[dstsize + i] = '\0';
-	return (dstsize + ft_strlen(src));
+	return (dstsize + srcsize + 1);
 }
-// int main()
-// {
-// 	char dst[30] = "there is no stars in the sky";
-// 	char dst1[30] = "there is no stars in the sky";
-// 	char const *src = "hello";
-// 	size_t size = ft_strlen(src) + ft_strlen(dst);
+int main()
+{
+	char dst[30] = "there is no stars in the sky";
+	char dst1[30] = "there is no stars in the sky";
+	char const *src = "hello";
+	size_t size = ft_strlen(src) + ft_strlen(dst);
 
-//     size_t a = strlcat(dst, src, size);
-//     size_t b = ft_strlcat(dst1, src, size);
-//     printf("strlcat\t\t====>%s| return %zu\n", dst, a);
-//     printf("ft_strlcat\t====>%s| return %zu\n", dst1, b);
-// }
+    size_t a = strlcat(dst, src, size);
+    size_t b = ft_strlcat(dst1, src, size);
+    printf("strlcat\t\t====>%s| return %zu\n", dst, a);
+    printf("ft_strlcat\t====>%s| return %zu\n", dst1, b);
+}
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,30 +6,50 @@
 /*   By: hfegrach <hfegrach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 01:26:35 by marvin            #+#    #+#             */
-/*   Updated: 2024/10/31 14:19:27 by hfegrach         ###   ########.fr       */
+/*   Updated: 2024/11/01 09:50:28 by hfegrach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// char	*ft_strrchr(const char *str, int c)
+// {
+// 	size_t	i;
+
+// 	i = ft_strlen(str);
+// 	while (i > 0)
+// 	{
+// 		i--;
+// 		if (*((unsigned char *)str + i) == (unsigned char)c)
+// 			return ((char *)str + i);
+// 	}
+// 	return (NULL);
+// }
+
 char	*ft_strrchr(const char *str, int c)
 {
-	size_t	i;
-
-	i = ft_strlen(str);
-	while (i > 0)
+	int i;
+	int tmp;
+	
+	tmp = 0;
+	i = 0;
+	while (str[i])
 	{
-		i--;
-		if (*((unsigned char *)str + i) == (unsigned char)c)
-			return ((char *)str + i);
+		if (str[i] == c)
+			tmp = i;
+		i++;
 	}
-	return (NULL);
+	if (c == '\0')
+		return ((char *)str + i);
+	if (!tmp)
+		return (NULL);
+	return ((char *)str + tmp);
 }
 
 // int main()
 // {
-//     const char *str = "hello world!\0\0";
-//     const char *str1 = "hello world!\0\0";
+//     const char *str = "hello world!\0\0\0";
+//     const char *str1 = "hello world!\0\0\0";
 //     char c = '\0';
 //     char *x = ft_strrchr(str, c);
 //     char *y = strrchr(str1, c);
