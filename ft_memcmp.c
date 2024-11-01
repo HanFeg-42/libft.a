715d0f306a40ff52ca5fdcde210be06a107bcdf6
HanFeg-42 <hananefegrachi2002@gmail.com>
2024-11-01 21:36:15 +0100

    soukaina is the best

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hfegrach <hfegrach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/26 15:41:03 by marvin            #+#    #+#             */
-/*   Updated: 2024/11/01 11:40:51 by hfegrach         ###   ########.fr       */
+/*   Created: 2024/10/2size 15:41:03 by marvin            #+#    #+#             */
+/*   Updated: 2024/11/01 14:41:59 by hfegrach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,18 +32,25 @@ int ft_memcmp(const void *str1, const void *str2, size_t n)
 {
     size_t i;
 
+    if (!n)
+        return (0);
     i = 0;
     while (i < n && *((unsigned char *)str1 + i) == *((unsigned char *)str2 + i))
+    {
+        if (i == n - 1)
+            return (*((unsigned char *)str1 + i) - *((unsigned char *)str2 + i));
         i++;
+    }
     return (*((unsigned char *)str1 + i) - *((unsigned char *)str2 + i));
 }
 
 // int main()
 // {
-//     char s1[] = "abcde";
-//     char s2[] = "abcdeg";
-//     int result = ft_memcmp(s1, s2, 6);
-//     int result1 = memcmp(s1, s2, 6);
+//     char s1[] = "zyxbcdefgh";
+//     char s2[] = "abcdefgxyz";
+//     int size = 0;
+//     int result = ft_memcmp(s1, s2, size);
+//     int result1 = memcmp(s1, s2, size);
 
 //     printf("ft_memcmp\t==> %d\n", result);
 //     printf("memcmp\t\t==> %d\n", result1);
