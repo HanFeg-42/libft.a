d4d06c46864058034e9b2e190de5e4ca88a4a529
HanFeg-42 <hananefegrachi2002@gmail.com>
2024-11-02 20:44:17 +0100

    substr ok

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: hfegrach <hfegrach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 00:37:57 by marvin            #+#    #+#             */
-/*   Updated: 2024/11/01 13:30:51 by hfegrach         ###   ########.fr       */
+/*   Updated: 2024/11/02 20:42:16 by hfegrach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ char	*ft_strchr(const char *s, int c)
 
 // int main()
 // {
-//     const char *str = "teste";
-//     const char *str1 = "teste";
-//     char c = 'e';
+//     const char *str = "tes\0te";
+//     const char *str1 = "tes\0te";
+//     char c = '\0';
 //     char * a = ft_strchr(str, c);
 //     char * b = strchr(str1, c);
 
