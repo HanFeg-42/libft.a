812b4a09cd4eb49cd59379d5bdd951006dfcd11e
HanFeg-42 <hananefegrachi2002@gmail.com>
2024-11-06 14:29:03 +0100

    bonus test

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -34,53 +34,66 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	return (new_elmt);
 }
 
-// void del(void *content) {
-//     free(content); // Free the integer pointer
-// }
+void del(void *content) {
+    free(content); // Free the pointer
+}
 
-// void *increment(void *content) {
-//     if (!content) return NULL;
-//     int *new_value = malloc(sizeof(int));
-//     if (!new_value) return NULL; // Handle memory allocation failure
-//     *new_value = *(int *)content + 1; // Increment the integer
-//     return new_value; // Return new incremented value
-// }
+void *f(void *content)
+{
+    char *str = (char *)content;
+    char *new_str = ft_strdup(str);  // Create a copy of the string
+    if (!new_str)
+        return NULL;  // Handle memory allocation failure
 
-// int main()
-// {
-// 	t_list *new;
-// 	t_list *current;
-// 	t_list *last;
-// 	char s1[] = " ana first new ";
-// 	char s2[] = " ana add back ";
-// 	char s3[] = " ana add front";
-// 	char s5[] = " ana last node";
-// 	char s4[] = " to be delone";
+    for (int i = 0; new_str[i]; i++) {
+        new_str[i] = ft_toupper((unsigned char)new_str[i]);  // Convert each character to uppercase
+    }
 
-// 	new = ft_lstnew(s1);
-// 	ft_lstadd_back(&new, ft_lstnew(s2));
-// 	ft_lstadd_front(&new, ft_lstnew(s3));
-// 	ft_lstadd_back(&new, ft_lstnew(s4));
-// 	ft_lstadd_back(&new, ft_lstnew(s5));
-// 	last = ft_lstlast(new);
-// 	printf("last = %s\n", (char*)(last->content));
-// 	printf("size = %d\n", ft_lstsize(new));
-// 	current = new;
-// 	while (current)
-// 	{
-// 		printf("%s\t", (char*)(current->content));
-// 		current = current->next;
-// 	}
-// 	ft_lstdelone(last, del);
-// 	// printf("after delone\n");
-// 	// current = new;
-// 	// while (current)
-// 	// {
-// 	// 	printf("%s\t", (char*)(current->content));
-// 	// 	current = current->next;
-// 	// }
-// 	// free(new);
-// }
+    return new_str;
+}
+
+
+int main()
+{
+	t_list *map;
+	t_list *new;
+	t_list *current;
+	t_list *tmp;
+	t_list *last;
+	char *s1 = ft_strdup(" ana first new ");
+	char *s2 = ft_strdup(" ana add back ");
+	char *s3 = ft_strdup(" ana add front");
+	char *s4 = ft_strdup(" ana last node");
+	char *s5 = ft_strdup(" to be delone");
+
+	new = ft_lstnew(s1);
+	ft_lstadd_back(&new, ft_lstnew(s2));
+	ft_lstadd_front(&new, ft_lstnew(s3));
+	ft_lstadd_back(&new, ft_lstnew(s4));
+	tmp = ft_lstlast(new);
+	ft_lstadd_back(&new, ft_lstnew(s5));
+	last = ft_lstlast(new);
+	printf("last = %s\n", (char*)(last->content));
+	printf("size = %d\n", ft_lstsize(new));
+	current = new;
+	while (current)
+	{
+		printf("%s\t", (char*)(current->content));
+		current = current->next;
+	}
+	ft_lstdelone(last, del);
+	tmp->next = NULL;
+	printf("\n\nafter delone\n");
+	//ft_lstiter(new, f);
+	map = ft_lstmap(new, f, del);
+	current = map;
+	while (current)
+	{
+		printf("%s\t", (char*)(current->content));
+		current = current->next;
+	}
+	ft_lstclear(&new, del);
+}
 
 
 // t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
