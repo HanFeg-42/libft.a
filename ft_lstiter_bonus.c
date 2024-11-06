c466d4eddc79bdfbfd4c8711789be98eab6628e3
HanFeg-42 <hananefegrachi2002@gmail.com>
2024-11-06 16:20:36 +0100

    bonus done

--- ft_lstiter_bonus.c
+++ ft_lstiter_bonus.c
@@ -23,54 +23,24 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 	}
 }
 
-void del(void *content) {
-    free(content); // Free the pointer
-}
-
-void f(void *content)
-{
-    char *str = (char *)content;
-    char **new_str = (char **)ft_strdup(str);  // Create a copy of the string
-
-    for (int i = 0; new_str[i]; i++) {exit(0);
-        new_str[i] = (unsigned char)ft_toupper((unsigned char)new_str[i]);  // Convert each character to uppercase
-    }
-}
-
-int main()
-{
-	t_list *tmp;
-	char *s1 = ft_strdup("salam");
-	char *s2 = ft_strdup("zahia");
-
-	t_list *lst = ft_lstnew(s1);
-	ft_lstadd_back(&lst, ft_lstnew(s2));
-	ft_lstiter(lst, f);
-	tmp = lst;
-	while(tmp)
-	{
-		printf("%s\n", (char*)(tmp->content));
-		tmp = tmp->next;
-	}
-	ft_lstclear(&lst, del);
-}
-
-// void f(void *s)
-// {
-//     char **ret = (char **)s;
-//     *ret = malloc(sizeof(char) * 5);
-//     if (*ret)
-//         strcpy(*ret, "wach");
+// void del(void *content) {
+//     free(content); // Free the pointer
 // }
-// //pass the adress of lst->content if u wanna change content to "wach";
+
+// void ft_print(void *content)
+// {
+// 	char *s = (char*)content;
+
+// 	printf("%s\n", s);
+// }
+
 // int main()
 // {
-
-//     t_list *new = malloc(sizeof(t_list));
-//     new->content = "wach_new";
-//     new->next = NULL;
-//     ft_lstiter(new, f);
-//     printf("%s\n", new->content);
-
-//     return (0);
+// 	char *s1 = ft_strdup("salam");
+// 	char *s2 = ft_strdup("zahia");
+// 	t_list *lst = ft_lstnew(s1);
+	
+// 	ft_lstadd_back(&lst, ft_lstnew(s2));
+// 	ft_lstiter(lst, ft_print);
+// 	ft_lstclear(&lst, del);
 // }
