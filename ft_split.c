fc1973514ea6ed4acc450e8ad1f73c8f3ee110b8
Hanane Feg <hananefegrachi@gmail.com>
2024-11-09 02:40:46 +0100

    split maybe done --

--- ft_split.c
+++ ft_split.c
@@ -12,79 +12,156 @@
 
 #include "libft.h"
 
-static int	ft_countwords(char const *s, char c)
+static int ft_countword(char const *s, char c)
 {
-	int	count;
-	int	i;
-	int	j;
+    int count;
+    int i;
+    int j;
 
-	count = 0;
-	i = 0;
-	j = 1;
-	while (s[i])
-	{
-		if (s[i] == c)
-			j = 1;
-		else if (j == 1 && s[i])
-		{
-			count++;
-			j = 0;
-		}
-		i++;
-	}
-	return (count);
+    count = 0;
+    j = 1;
+    i = 0;
+    while (s[i])
+    {
+        if (s[i] == c)
+            j = 1;
+        else if (s[i] != c && j == 1)
+        {
+            count++;
+            j = 0;
+        }
+        i++;
+    }
+    return (count);
 }
 
-static char	*slices(char const *s, char c, int i)
+static char **ft_freeMe(char **s, int index)
 {
-	char	*str;
-	int		j;
-	int		len;
-
-	len = 0;
-	while (s[i + len] && s[i + len] != c)
-		len++;
-	str = malloc(sizeof(char) * len + 1);
-	if (!str)
-		return (NULL);
-	j = 0;
-	while (s[i] && s[i] != c)
-	{
-		str[j++] = s[i++];
-	}
-	str[j] = '\0';
-	return (str);
+    while (index >= 0)
+    {
+        free(s[index]);
+        index--;
+    }
+    free(s);
+    return (NULL);
 }
 
-char	**ft_split(char const *s, char c)
+static char **ft_slices(char **ret, char const *s, char c,int a, int i, int j)
 {
-	char	**ret;
-	int		i;
-	int		j;
-	int		a;
+    int len;
+
+    while (s[i])
+    {
+        if (s[i] == c)
+            j = 1;
+        else if (s[i] != c && j == 1)
+        {
+            len = 0;
+            while (s[i + len] && s[i + len] != c)
+                len++;
+            ret[a] = ft_substr(s, i, len);
+            if (!ret[a])
+                return (ft_freeMe(ret, a));
+            a++;
+            j = 0;
+        }
+        i++;
+    }
+    ret[a] = NULL;
+    return (ret);
+}
+
+char **ft_split(char const *s, char c)
+{
+    char **ret;
+    int a;
+    int i;
+    int j;
+    
+    a = 0;
+    j = 1;
+    i = 0;
+    if (!s)
+        return (NULL);
+    ret = malloc(sizeof(char *) * (ft_countword(s, c) + 1));
+    if (!ret)
+        return (NULL);
+    ret = ft_slices(ret, s, c, a, i, j);
+    return (ret);
+}
+// static int	ft_countwords(char const *s, char c)
+// {
+// 	int	count;
+// 	int	i;
+// 	int	j;
+
+// 	count = 0;
+// 	i = 0;
+// 	j = 1;
+// 	while (s[i])
+// 	{
+// 		if (s[i] == c)
+// 			j = 1;
+// 		else if (j == 1 && s[i])
+// 		{
+// 			count++;
+// 			j = 0;
+// 		}
+// 		i++;
+// 	}
+// 	return (count);
+// }
+
+// static char	*slices(char const *s, char c, int i)
+// {
+// 	char	*str;
+// 	int		j;
+// 	int		len;
+
+// 	len = 0;
+// 	while (s[i + len] && s[i + len] != c)
+// 		len++;
+// 	str = malloc(sizeof(char) * len + 1);
+// 	if (!str)
+// 		return (NULL);
+// 	j = 0;
+// 	while (s[i] && s[i] != c)
+// 	{
+// 		str[j++] = s[i++];
+// 	}
+// 	str[j] = '\0';
+// 	return (str);
+// }
+
+// char	**ft_split(char const *s, char c)
+// {
+// 	char	**ret;
+// 	int		i;
+// 	int		j;
+// 	int		a;
 	
-	if (!s)
-		return (NULL);
-	ret = malloc(sizeof(char *) * (ft_countwords(s, c) + 1));
-	if (!ret)
-		return (NULL);
-	a = 0;
-	i = 0;
-	j = 1;
-	while (s[i])
-	{
-		if (s[i] == c)
-			j = 1;
-		else if (j == 1 && s[i])
-		{
-			ret[a++] = slices(s, c, i);
-			j = 0;
-		}
-		i++;
-	}
-	ret[a] = NULL;
-	return (ret);
-}
+// 	if (!s)
+// 		return (NULL);
+// 	ret = malloc(sizeof(char *) * (ft_countwords(s, c) + 1));
+// 	if (!ret)
+// 		return (NULL);
+// 	a = 0;
+// 	i = 0;
+// 	j = 1;
+// 	while (s[i])
+// 	{
+// 		if (s[i] == c)
+// 			j = 1;
+// 		else if (j == 1 && s[i])
+// 		{
+// 			ret[a++] = slices(s, c, i);
+// 			j = 0;
+// 		}
+// 		i++;
+// 	}
+// 	ret[a] = NULL;
+// 	return (ret);
+// }
 
 // SSSSSSSSSPPPPPPPPPPLLLLLLLLLLLIIIIIIIIIIITTTTTTTTTTTT
 
@@ -99,6 +176,31 @@ char	**ft_split(char const *s, char c)
 // 	// exit(0);
 // 	i = 0;
 // 	while (i < ft_countwords(s, ' '))
+// 	{
+// 		printf("%s\n", sp[i++]);
+// 	}
+// 	return (0);
+// }
+
+
+
+// int	main(void)
+// {
+// 	char	*s;
+// 	char	**sp;
+// 	int		i;
+
+// 	s = NULL;
+// 	sp = ft_split(s, '\0');
+// 	if (sp == NULL)
+// 	{
+// 		printf("Error: ft_split returned NULL\n");
+// 		return (1);  // return error code
+// 	}
+// 	// exit(0);
+// 	//printf("%d\nkkk\n", ft_countword(s, '\0'));
+// 	i = 0;
+// 	while (i < ft_countword(s, '\0'))
 // 	{
 // 		printf("%s\n", sp[i++]);
 // 	}
