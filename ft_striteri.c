7601689c4788003277f05b311b64019c67e7dc26
HanFeg-42 <hananefegrachi2002@gmail.com>
2024-11-11 15:16:34 +0100

    wellah ila 3yiit

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: hfegrach <hfegrach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 16:14:57 by hfegrach          #+#    #+#             */
-/*   Updated: 2024/10/31 05:03:26 by hfegrach         ###   ########.fr       */
+/*   Updated: 2024/11/11 15:16:08 by hfegrach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,3 +25,16 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 		i++;
 	}
 }
+
+// void upper(unsigned int i, char *c)
+// {
+// 	i++;
+// 	*c = ft_toupper(*c);
+// }
+
+// int main()
+// {
+// 	char *s = ft_strdup("salam wash akhay sat");
+// 	ft_striteri(s, upper);
+// 	printf("%s", s);
+// }
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: hfegrach <hfegrach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 15:37:04 by hfegrach          #+#    #+#             */
-/*   Updated: 2024/10/31 05:05:05 by hfegrach         ###   ########.fr       */
+/*   Updated: 2024/11/11 15:06:30 by hfegrach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,3 +31,18 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	ret[i] = '\0';
 	return (ret);
 }
+
+// char upper(unsigned int i, char c)
+// {
+// 	i++;
+// 	return ft_toupper(c);
+// }
+
+// int main()
+// {
+// 	char const *s = "salam wash akhay sat";
+	
+// 	char *str = ft_strmapi(s, upper);
+	
+// 	printf("%s", str);
+// }
